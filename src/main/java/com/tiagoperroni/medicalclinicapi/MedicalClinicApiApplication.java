ab0c0303ab858c9ea68ccf8efb8503d35db9ff2b
Tiago <tiagoperroni86@gmail.com>
2023-01-25 10:34:43 -0300

    start project add h2database

---
+++ src/main/java/com/tiagoperroni/medicalclinicapi/MedicalClinicApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiagoperroni.medicalclinicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalClinicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagoperroni/medicalclinicapi/MedicalClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagoperroni.medicalclinicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
